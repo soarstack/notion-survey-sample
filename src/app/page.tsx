a4c3d6b5aba5306361c76176c8aacab978445f33
Stefan Ciprian Hotoleanu <hotoleanu.ciprian@gmail.com>
2024-05-31 16:45:21 +0300

    Forgot use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import dynamic from 'next/dynamic';
+'use client'
 
-const SurveyJS = dynamic(() => import('../components/SurveyJS'), { ssr: false });
+import SurveyJS from './components/SurveyJS';
+
 
 export default function Home() {
   return (
