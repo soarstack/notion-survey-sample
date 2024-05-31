975fcdae7251cbb0c242e363b53b1c54ca591631
Stefan Ciprian Hotoleanu <hotoleanu.ciprian@gmail.com>
2024-05-31 20:36:53 +0300

    chore: Update page title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main>
       <div className="container mx-auto px-4">
         <div className="max-w-2xl mx-auto text-center">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">SurveyJS with Notion Database</h1>
+          {/* <h1 className="text-3xl font-bold text-gray-800 mb-4">SurveyJS with Notion Database</h1> */}
           <SurveyJS />
         </div>
       </div>
